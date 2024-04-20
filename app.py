ce042227f8386591d7b0d359a01b51dbe900a194
Md Adnan Baqi <144843601+adnanbaqi@users.noreply.github.com>
2024-04-20 20:41:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
-from fastapi import FastAPI
+import logging
+from fastapi import FastAPI, Response, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from controller.store_data import router as store_router
-from controller.load_data  import router as load_router  # Importing from the controller directory
+from controller.load_data import router as load_router  # Importing from the controller directory
 # from controller.train_data import router as train_router
 
+# Setup logging
+logging.basicConfig(level=logging.INFO, filename='LOGS/endpoint.log', filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = FastAPI()
 
+# Setup CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"],  
     allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
+# Logging that CORS middleware has been added
+logging.info("CORS middleware configured.")
 
-
+# Include routerss
 app.include_router(store_router)
 app.include_router(load_router)
 # app.include_router(train_router)
 
-
-
 @app.get("/", response_class=HTMLResponse)
 async def get_home():
+    logging.info("Home page accessed.")
     return '''
 <!DOCTYPE html>
 <html>
@@ -46,3 +54,5 @@ async def get_home():
 </body>
 </html>
 '''
+
+logging.info("FastAPI application has been configured.")
